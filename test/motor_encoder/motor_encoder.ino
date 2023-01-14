99a9cbec1a7d3a0c28d103d4ed509bf1afa16e4d
Piotr Smarzynski <smarzony@gmail.com>
2023-01-14 14:26:30 +0100

    calibrated motors, ready for balancing calibration

--- test/motor_encoder/motor_encoder.ino
+++ test/motor_encoder/motor_encoder.ino
@@ -1,18 +1,36 @@
 //The sample code for driving one way motor encoder
 #include <PID_v1.h>
-const byte encoder0pinA = 2;  //A pin -> the interrupt pin 0
-const byte encoder0pinB = 3;  //B pin -> the digital pin 3
-int E_right = 6;              //The enabling of L298PDC motor driver board connection to the digital interface port 5
-int M_right = 7;              //The enabling of L298PDC motor driver board connection to the digital interface port 4
-byte encoder0PinALast;
-double duration, abs_duration;  //the number of the pulses
-boolean Direction;              //the rotation direction
-boolean result;
+#define encoder_L_pinA 2  //A pin -> the interrupt pin 0
+#define encoder_L_pinB 8  //B pin -> the digital pin 3
+#define encoder_R_pinA 3  //A pin -> the interrupt pin 0
+#define encoder_R_pinB 9  //B pin -> the digital pin 3
 
-double val_output;  //Power supplied to the motor PWM value.
-double Setpoint;
-double Kp = 0.6, Ki = 5, Kd = 0;
-PID myPID(&duration, &val_output, &Setpoint, Kp, Ki, Kd, DIRECT);
+#define Motor_L_DIR_pin 4
+#define Motor_L_PWM_pin 5
+
+#define Motor_R_PWM_pin 6
+#define Motor_R_DIR_pin 7
+
+#define MOTOR_SETPOINT_STEP 10
+#define PID_SAMPLE_TIME 30
+#define SPEED_SETPOINT_LIMIT 4*PID_SAMPLE_TIME
+
+bool encoder_L_PinALast, encoder_R_PinALast;
+double pulses_left, pulses_right, abs_pulses_left;  //the number of the pulses
+boolean dir_left, dir_right;                        //the rotation dir_left
+boolean motor_left_pid_ready, motor_right_pid_ready;
+
+double motor_left_pwm;  //Power supplied to the motor PWM value.
+double motor_left_setpoint_speed;
+
+double motor_right_pwm;  //Power supplied to the motor PWM value.
+double motor_right_setpoint_speed;
+
+double Kp = 5, 
+       Ki = 8, 
+       Kd = 0.2;
+PID motor_right_pid(&pulses_right, &motor_right_pwm, &motor_right_setpoint_speed, Kp, Ki, Kd, REVERSE);
+PID motor_left_pid(&pulses_left, &motor_left_pwm, &motor_left_setpoint_speed, Kp, Ki, Kd, REVERSE);
 
 int adc_key_val[5] = { 50, 200, 400, 600, 800 };
 int NUM_KEYS = 5;
@@ -20,81 +38,132 @@ int adc_key_in;
 int key = -1;
 int oldkey = -1;
 
+unsigned long long now, serial_print_time;
+
 void setup() {
-  Serial.begin(115200);      //Initialize the serial port
-  pinMode(M_right, OUTPUT);  //L298P Control port settings DC motor driver board for the output mode
-  pinMode(E_right, OUTPUT);
-  Setpoint = 0;            //Set the output value of the PID
-  myPID.SetMode(AUTOMATIC);  //PID is set to automatic mode
-  myPID.SetSampleTime(100);  //Set PID sampling frequency is 100ms
-  myPID.SetOutputLimits(-255, 255);
-  EncoderInit();  //Initialize the module
+  Serial.begin(115200);  //Initialize the serial port
+
+  pinMode(Motor_R_DIR_pin, OUTPUT);  //L298P Control port settings DC motor driver board for the output mode
+  pinMode(Motor_R_PWM_pin, OUTPUT);
+  pinMode(Motor_L_DIR_pin, OUTPUT);  //L298P Control port settings DC motor driver board for the output mode
+  pinMode(Motor_L_PWM_pin, OUTPUT);
+
+  motor_right_setpoint_speed = 0;
+  motor_left_setpoint_speed = 0;  //Set the output value of the PID
+
+  motor_right_pid.SetMode(AUTOMATIC);              //PID is set to automatic mode
+  motor_right_pid.SetSampleTime(PID_SAMPLE_TIME);  //Set PID sampling frequency is 100ms
+  motor_right_pid.SetOutputLimits(-255, 255);
+
+  motor_left_pid.SetMode(AUTOMATIC);              //PID is set to automatic mode
+  motor_left_pid.SetSampleTime(PID_SAMPLE_TIME);  //Set PID sampling frequency is 100ms
+  motor_left_pid.SetOutputLimits(-255, 255);
+
+  encoder_left_init();   //Initialize the module
+  encoder_right_init();  //Initialize the module
 }
 
 void loop() {
+  now = millis();
   keyboard_read();
-  advance();  //Motor Forward
-  //abs_duration = abs(duration);
-  result = myPID.Compute();  //PID conversion is complete and returns 1
-  if (result) {
-    Serial.print("Pulse:");
-    Serial.print(duration);
+
+  motor_left_go();   //Motor Forward
+  motor_right_go();  //Motor Forward
+
+  motor_right_pid_ready = motor_right_pid.Compute();  //PID conversion is complete and returns 1
+  motor_left_pid_ready = motor_left_pid.Compute();    //PID conversion is complete and returns 1
+  if (motor_left_pid_ready or motor_right_pid_ready) {
+
+    Serial.print("PL:");
+    Serial.print(pulses_left);
     Serial.print(",");
-    Serial.print("pwm:");
-    Serial.print(val_output);
+    Serial.print("MLP:");
+    Serial.print(motor_left_pwm);
     Serial.print(",");
-    Serial.print("setpoint:");
-    Serial.println(Setpoint);
-    duration = 0;  //Count clear, wait for the next count
+    Serial.print("MLS:");
+    Serial.print(motor_left_setpoint_speed);
+
+    Serial.print("PR:");
+    Serial.print(pulses_right);
+    Serial.print(",");
+    Serial.print("MRP:");
+    Serial.print(motor_right_pwm);
+    Serial.print(",");
+    Serial.print("MRS:");
+    Serial.print(motor_right_setpoint_speed);
+    Serial.println();
+
+    if (motor_left_pid_ready)
+      pulses_left = 0;  //Count clear, wait for the next count
+    if (motor_right_pid_ready)
+      pulses_right = 0;  //Count clear, wait for the next count
   }
 }
 
-void EncoderInit() {
-  Direction = true;  //default -> Forward
-  pinMode(encoder0pinB, INPUT);
-  attachInterrupt(digitalPinToInterrupt(encoder0pinA), wheelSpeed, CHANGE);
+void encoder_left_init() {
+  dir_left = true;  //default -> Forward
+  pinMode(encoder_L_pinB, INPUT);
+  attachInterrupt(digitalPinToInterrupt(encoder_L_pinA), wheel_left_pulse_count, CHANGE);
 }
 
-void wheelSpeed() {
-  int Lstate = digitalRead(encoder0pinA);
-  if ((encoder0PinALast == LOW) && Lstate == HIGH) {
-    int val = digitalRead(encoder0pinB);
-    if (val == LOW && Direction) {
-      Direction = false;  //Reverse
-    } else if (val == HIGH && !Direction) {
-      Direction = true;  //Forward
+void encoder_right_init() {
+  dir_right = true;  //default -> Forward
+  pinMode(encoder_R_pinB, INPUT);
+  attachInterrupt(digitalPinToInterrupt(encoder_R_pinA), wheel_right_pulse_count, CHANGE);
+}
+
+void wheel_left_pulse_count() {
+  bool Lstate = digitalRead(encoder_L_pinA);
+  if ((encoder_L_PinALast == LOW) && Lstate == HIGH) {
+    bool val = digitalRead(encoder_L_pinB);
+    if (val == LOW && dir_left) {
+      dir_left = false;  //Reverse
+    } else if (val == HIGH && !dir_left) {
+      dir_left = true;  //Forward
     }
   }
-  encoder0PinALast = Lstate;
+  encoder_L_PinALast = Lstate;
 
-  if (!Direction) duration++;
-  else duration--;
+  if (!dir_left) pulses_left--;
+  else pulses_left++;
 }
-void advance()  //Motor Forward
-{
-  if (val_output > 0)
-  {
-    digitalWrite(M_right, LOW);
-    analogWrite(E_right, val_output);
+
+void wheel_right_pulse_count() {
+  int Rstate = digitalRead(encoder_R_pinA);
+  if ((encoder_R_PinALast == LOW) && Rstate == HIGH) {
+    bool val = digitalRead(encoder_R_pinB);
+    if (val == LOW && dir_right) {
+      dir_right = false;  //Reverse
+    } else if (val == HIGH && !dir_right) {
+      dir_right = true;  //Forward
+    }
   }
-  else
-  {
-    digitalWrite(M_right, HIGH);
-    analogWrite(E_right, abs(val_output));
+  encoder_R_PinALast = Rstate;
+
+  if (!dir_right) pulses_right--;
+  else pulses_right++;
+}
+
+void motor_right_go()  //Motor Forward
+{
+  if (motor_right_pwm > 0) {
+    digitalWrite(Motor_R_DIR_pin, LOW);
+    analogWrite(Motor_R_PWM_pin, (uint8_t)motor_right_pwm);
+  } else {
+    digitalWrite(Motor_R_DIR_pin, HIGH);
+    analogWrite(Motor_R_PWM_pin, (uint8_t)abs(motor_right_pwm));
   }
-
-
-  
-}
-void back()  //Motor reverse
-{
-  digitalWrite(M_right, HIGH);
-  analogWrite(E_right, val_output);
 }
 
-void Stop()  //Motor stops
+void motor_left_go()  //Motor Forward
 {
-  digitalWrite(E_right, LOW);
+  if (motor_left_pwm > 0) {
+    digitalWrite(Motor_L_DIR_pin, LOW);
+    analogWrite(Motor_L_PWM_pin, motor_left_pwm);
+  } else {
+    digitalWrite(Motor_L_DIR_pin, HIGH);
+    analogWrite(Motor_L_PWM_pin, abs(motor_left_pwm));
+  }
 }
 
 void keyboard_read() {
@@ -111,27 +180,46 @@ void keyboard_read() {
         digitalWrite(13, HIGH);
         switch (key) {  // Send messages accordingly.
           case 0:
-            Serial.println("Setpoint 400");
+            // Serial.println("motors_setpoint_speed 0");
+            motor_left_setpoint_speed = 0;
+            motor_right_setpoint_speed = 0;
 
-            Setpoint = 400;
-            if (Setpoint > 440)
-              Setpoint = 440;
             break;
           case 1:
-            Serial.println("Setpoint -400");
-            Setpoint = -400;
-            if (Setpoint < -440)
-              Setpoint = -440;
+            // Serial.print("motor_right_setpoint_speed +");
+            // Serial.println(MOTOR_SETPOINT_STEP);
+            // motor_right_setpoint_speed = motor_right_setpoint_speed + MOTOR_SETPOINT_STEP;
+            motor_right_setpoint_speed = SPEED_SETPOINT_LIMIT - SPEED_SETPOINT_LIMIT*0.6;
+            if (motor_right_setpoint_speed > SPEED_SETPOINT_LIMIT) {
+              motor_right_setpoint_speed = SPEED_SETPOINT_LIMIT;
+            }
             break;
           case 2:
-            Serial.println("Setpoint 0");
-            Setpoint = 0;
+            // Serial.print("motor_right_setpoint_speed -");
+            // Serial.println(MOTOR_SETPOINT_STEP);
+            // motor_right_setpoint_speed = motor_right_setpoint_speed - MOTOR_SETPOINT_STEP;
+            motor_right_setpoint_speed = -SPEED_SETPOINT_LIMIT + SPEED_SETPOINT_LIMIT*0.6;
+            if (motor_right_setpoint_speed < -SPEED_SETPOINT_LIMIT) {
+              motor_right_setpoint_speed = -SPEED_SETPOINT_LIMIT;
+            }
             break;
           case 3:
-            Serial.println("S4 OK");
+            // Serial.print("motor_left_setpoint_speed -");
+            // Serial.println(MOTOR_SETPOINT_STEP);
+            motor_left_setpoint_speed = -SPEED_SETPOINT_LIMIT + SPEED_SETPOINT_LIMIT*0.6;
+            // motor_left_setpoint_speed = motor_left_setpoint_speed - MOTOR_SETPOINT_STEP;
+            if (motor_right_setpoint_speed < -SPEED_SETPOINT_LIMIT) {
+              motor_right_setpoint_speed = -SPEED_SETPOINT_LIMIT;
+            }
             break;
           case 4:
-            Serial.println("S5 OK");
+            // Serial.print("motor_left_setpoint_speed +");
+            // Serial.println(MOTOR_SETPOINT_STEP);
+            motor_left_setpoint_speed = SPEED_SETPOINT_LIMIT - SPEED_SETPOINT_LIMIT*0.6;
+            // motor_left_setpoint_speed = motor_left_setpoint_speed + MOTOR_SETPOINT_STEP;
+            if (motor_right_setpoint_speed > SPEED_SETPOINT_LIMIT) {
+              motor_right_setpoint_speed = SPEED_SETPOINT_LIMIT;
+            }
             break;
         }
       }
